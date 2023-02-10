7531fa0672820bf4378963751197968ad3cd1485
P.Ivo <pivolluz@gmail.com>
2023-02-09 22:10:12 -0300

    project created

---
+++ src/main/java/com/pivo/workShop/WorkShopApplication.java
@@ -0,0 +1,13 @@
+package com.pivo.workShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pivo/workShop/WorkShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pivo.workShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
