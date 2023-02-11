c78873879716c3feae288b6bb631b477401e4103
P.Ivo <pivolluz@gmail.com>
2023-02-10 22:17:10 -0300

    user entity and resource

---
+++ src/main/java/com/pivo/workShop/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.pivo.workShop.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pivo.workShop.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@email","9999999" , "124568");
+		return ResponseEntity.ok().body(u);
+		
+	}
+}
